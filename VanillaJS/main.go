63ae2557c0b9d220d42d7b68121e540f4bfbddb4
klooopmoo <gc.franco93@gmail.com>
2022-06-10 15:36:28 -0700

    adding api put logic

--- VanillaJS/main.go
+++ VanillaJS/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -10,10 +11,14 @@ type State struct {
 	Counter int `json:"counter"`
 }
 
-func main() {
+type Error struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
 
-	http.HandleFunc("/counter", handleCounter())
-	http.Handle("/", http.FileServer(http.Dir("./build")))
+func main() {
+	http.HandleFunc("/state", handleState())
+	http.Handle("/", http.FileServer(http.Dir("./public")))
 
 	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
@@ -21,12 +26,15 @@ func main() {
 	}
 }
 
-func handleCounter() func(w http.ResponseWriter, r *http.Request) {
+func handleState() func(w http.ResponseWriter, r *http.Request) {
 	state := &State{
 		Counter: 5,
 	}
+	errState := &Error{}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		b, err := json.Marshal(*state)
+
 		if err != nil {
 			fmt.Println("unable to marshal state", err)
 		}
@@ -34,6 +42,37 @@ func handleCounter() func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
 			_, err := w.Write(b)
+			if err != nil {
+
+				fmt.Println("was not able to write state", err)
+			}
+		case "PUT":
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			err = json.Unmarshal(body, state)
+			if err != nil {
+				errState.Code = 400
+				errState.Message = "Input format must be type int"
+				errorMessage, _ := json.Marshal(*errState)
+				w.WriteHeader(400)
+				_, err := w.Write(errorMessage)
+				if err != nil {
+					return
+				}
+				return
+			}
+
+			back, err := json.Marshal(*state)
+			if err != nil {
+				errState.Message = "Unable to marshall state struct"
+				fmt.Println("Unable to marshall state struct")
+				return
+			}
+			_, err = w.Write(back)
+
 			if err != nil {
 				fmt.Println("was not able to write state", err)
 			}
